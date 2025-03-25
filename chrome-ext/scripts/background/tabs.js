c18dff332fab02c1f255c99b5f9a13aae8e96639
Dario Hamidi <dario.hamidi@gmail.com>
2025-03-25 18:08:50 +0200

    refactor: use activeTab permission instead of tabs

--- chrome-ext/scripts/background/tabs.js
+++ chrome-ext/scripts/background/tabs.js
@@ -7,17 +7,31 @@ export function extractDomain(url) {
   }
 }
 
+async function getCurrentTab() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  return tab;
+}
+
 export function setupTabListeners(onDomainChange) {
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  chrome.tabs.onActivated.addListener(async (activeInfo) => {
+    const tab = await getCurrentTab();
+    if (tab?.url) {
+      const domain = extractDomain(tab.url);
+      if (domain) {
+        onDomainChange(tab.id, domain);
+      }
+    }
+  });
+
+  chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
     if (changeInfo.status === 'complete') {
-      chrome.tabs.get(tabId).then(tab => {
-        if (tab.url) {
-          const domain = extractDomain(tab.url);
-          if (domain) {
-            onDomainChange(tabId, domain);
-          }
+      const tab = await getCurrentTab();
+      if (tab?.id === tabId && tab.url) {
+        const domain = extractDomain(tab.url);
+        if (domain) {
+          onDomainChange(tabId, domain);
         }
-      });
+      }
     }
   });
 } 

280b0cd5d761ed8752e0db404565d32f881e27b9
Dario Hamidi <dario.hamidi@gmail.com>
2025-04-14 12:38:22 +0300

    chore: publish v1.1.0 of extension

--- chrome-ext/build.sh
+++ chrome-ext/build.sh
@@ -13,10 +13,11 @@ rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
 # Copy files
-cp -r assets scripts styles manifest.json "$OUTPUT_DIR/"
+cp -r popup.html assets scripts styles manifest.json "$OUTPUT_DIR/"
 
 # Create zip for Chrome Web Store
 cd "$OUTPUT_DIR"
 zip -r "../$ZIP_NAME" ./*
 
-echo "Build complete: $ZIP_NAME created" 
+echo "Build complete: $ZIP_NAME created"
+
